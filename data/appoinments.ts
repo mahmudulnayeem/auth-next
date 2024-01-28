94e365704870a9f34be46b64333e38eb3030dec3
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-01-28 21:30:38 +0600

    add appoinmtnet

---
+++ data/appoinments.ts
@@ -0,0 +1,96 @@
+"use server";
+
+import { db } from "@/lib/db";
+
+export const getAppointmentsByDoctorId = async (doctorId: string) => {
+  try {
+    const appointments = await db.appointment.findMany({
+      where: {
+        doctorId,
+      },
+    });
+    return appointments;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getAppointmentsByPatientId = async (patientId: string) => {
+  try {
+    const appointments = await db.appointment.findMany({
+      where: {
+        patientId,
+      },
+    });
+    return appointments;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getAppointmentById = async (id: string) => {
+  try {
+    const appointment = await db.appointment.findUnique({
+      where: {
+        id,
+      },
+    });
+    return appointment;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const createAppointment = async (data: any) => {
+  try {
+    const appointment = await db.appointment.create({
+      data,
+    });
+    return appointment;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const updateAppointment = async (id: string, data: any) => {
+  try {
+    const appointment = await db.appointment.update({
+      where: {
+        id,
+      },
+      data,
+    });
+    return appointment;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const deleteAppointment = async (id: string) => {
+  try {
+    const appointment = await db.appointment.delete({
+      where: {
+        id,
+      },
+    });
+    return appointment;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const changeAppointmentStatus = async (id: string, status: string) => {
+  try {
+    const appointment = await db.appointment.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
+    return appointment;
+  } catch (error) {
+    return null;
+  }
+};
