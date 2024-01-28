4dafc7885fcb2f7e8f8fe31379dba68f4b5fc893
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-01-28 11:07:06 +0600

    add two new api route

--- data/user.ts
+++ data/user.ts
@@ -25,3 +25,34 @@ export const getUserById = async (id: string) => {
     return null;
   }
 };
+
+export const makeAdmin = async (id: string) => {
+  try {
+    const newAdmin = await db.user.update({
+      where: {
+        id,
+      },
+      data: {
+        role: "ADMIN",
+      },
+    });
+    return newAdmin;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getAllUserWithOutAdmin = async () => {
+  try {
+    const allUsers = await db.user.findMany({
+      where: {
+        role: {
+          not: "ADMIN",
+        },
+      },
+    });
+    return allUsers;
+  } catch (error) {
+    return null;
+  }
+};
